f1a0aedec2914c04849770e6a32b24f00cd56b3f
Victor Cassimiro <cassimironet@gmail.com>
2023-06-05 12:35:29 -0300

    Modo de edição

---
+++ Administrador/cliente/home - Copia - Copia (3).php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+require_once ("menu.html");
+include("../conexao.php");
+?>
+
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="A layout example that shows off a responsive pricing table.">
+<link rel="stylesheet" href="/css/pure/pure-min.css">
+<link rel="stylesheet" href="/css/pure/grids-responsive-min.css">
+<link rel="stylesheet" href="https://unpkg.com/purecss@2.0.3/build/pure-min.css" integrity="sha384-cg6SkqEOCV1NbJoCu11+bm0NvBRc8IYLRGXkmNrqUBfTjmMYwNKPWBTIKyw9mHNJ" crossorigin="anonymous">
+<title>Controle Produção</title>	
+<link rel="stylesheet" href="../css/formstyle.css">
+
+<style>
+</style>
+
+</script>
+</head>
+      
+<body>
+	
+</body>
+</html>
---
+++ verificaAcesso.php
@@ -0,0 +1,8 @@
+<?php 
+session_start();
+
+if(!isset($_SESSION["login"]) || !isset($_SESSION["senha"])){
+	header("Location: ../index.html");
+exit;
+}
+?>
